32debe060fd9b1e55937f5342e37adb73a1f8792
SlemJosh <joshua.slem@gmail.com>
2023-09-24 23:19:40 -0600

    Added a couple questions and answers in a variable format to our stylesheet

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+// First we are going to list out our question as a variable array known as questions.  Each question is basically a card that has the question, options, and the answer, which will be hidden to the user at first.
+
+var questions = [
+    {
+        title:"Commonly used data types DO NOT include:",
+        choices: ["1. Strings", "2. Booleans", "3. Alerts", "4. Numbers"],
+        answer: "3. Alerts"
+    },
+    {
+        title:"The condition in an if / else statement is enclosed with _______.",
+        choices:["1. Quotes","2. Curly Brackets","3. Parenthesis","4. Square Brackets"],
+        answer: "3. Parenthesis"
+        
+    }
+]
+
